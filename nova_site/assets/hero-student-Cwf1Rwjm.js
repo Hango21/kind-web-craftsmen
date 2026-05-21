bd68d2f9bab9d8ed5fadfdcda277b89e7b03d1f4
Hamdi Mohammed Jibril <melikaebrahim0@gmail.com>
2026-05-21 08:36:45 +0300

    chore: setup .cpanel.yml for deployment and clean up repo

---
+++ nova_site/assets/hero-student-Cwf1Rwjm.js
@@ -0,0 +1,1 @@
+const s="/assets/students-collab-DhpFJ-qi.jpg",t="/assets/hero-student-DtoUH3Eu.jpg";export{s as c,t as h};
---
+++ nova_site/assets/secondary-CXm_8aVO.js
@@ -0,0 +1,1 @@
+const s="/assets/early-childhood-Bolx6HOp.jpg",a="/assets/primary-CTYydVS6.jpg",o="/assets/secondary-BXhKNo7D.jpg";export{s as e,a as p,o as s};
